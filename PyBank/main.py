bdde4bcd504af08425d3fed96781331a77452137
Cody_Carpenter <cody.carpenter20@gmail.com>
2023-08-23 22:39:25 -0600

    Python update

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,4 +1,18 @@
-import pandas as pd
 
-df = pd.read_csv("budget_data.csv")
-print(df)
+import pandas as pd 
+
+import os 
+
+import csv
+csvpath= os.path.join(r'PyBank\Resources\budget_data.csv')
+with open(csvpath) as csvfile:
+  csvreader=csv.reader(csvfile,delimiter=',')
+  print(csvreader)
+
+  for row in csvreader:
+    print(row)
+
+df=pd.read_csv(r'PyBank\Resources\budget_data.csv')
+
+
+
